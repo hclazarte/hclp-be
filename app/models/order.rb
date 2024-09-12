4340ae7eb68a393794f73a9a2c66cc09e5157de7
Lazarte, Hector <hlazarte@everfi.com>
2024-09-12 11:35:35 -0400

    borrar del stock de productos las ordenes

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,9 +6,22 @@ class Order < ApplicationRecord
 
   validates :order_items, presence: true, length: { minimum: 1, message: 'debe incluir al menos un item en su orden' }
 
+  after_create :adjust_product_stock
+
   # Calcula el total de la orden automáticamente
   def calculate_total
     self.total = order_items.sum { |item| item.quantity * item.price }
     save
+  end
+
+  private
+
+  # Método para ajustar el stock de los productos asociados a la orden
+  def adjust_product_stock
+    order_items.each do |item|
+      product = item.product
+      product.stock -= item.quantity
+      product.save
+    end
   end
 end
